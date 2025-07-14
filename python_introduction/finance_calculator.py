2a4ca7f592fffb80b2554ca401eda5af109ff707
Zachary <zacharykilifi@gmail.com>
2025-07-14 03:20:31 +0300

    Added finance_calculator.py to repository

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+income = int(input("Enter your monthly income: "))
+expenses = int(input("Enter your monthly expenses: "))
+
+monthly_savings = income - expenses
+
+print("Your monthly savings are: $", monthly_savings)
+
+interest_rate = 0.05
+
+projected_annual_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+
+print( "Projected savings after one year, with interest, is: $", projected_annual_savings)

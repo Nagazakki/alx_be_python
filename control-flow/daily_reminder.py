4fbc2d8a9b6deb77f8c43b5978b35a701d38ca99
Zachary <zacharykilifi@gmail.com>
2025-07-28 01:16:54 +0300

    edit

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
-task = input("Enter your daily task: ")
-priority = input("Enter the priority (high, medium, low): ")
-time_bound = input("Is this task time-bound? (yes/no): ")
+task = input("Enter your task: ")
+priority = input("Priority (high, medium, low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":

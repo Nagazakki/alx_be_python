892afbd4d2aeba59213bb5f998fbe738d4fdd052
Zachary <zacharykilifi@gmail.com>
2025-08-05 10:04:38 +0300

    Added test_simple_calculator,py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,29 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    
+    def test_add(self):
+        result = self.calc.add(2, 3)
+        assert result == 5
+    
+    def test_subtract(self):
+        result = self.calc.subtract(10, 5)
+        assert result == 5
+    
+    def test_multiply(self):
+        result = self.calc.multiply (3, 4)
+        assert result == 12
+    
+    def test_divide(self):
+        result = self.calc.divide(10, 2)
+        assert result == 5
+    
+    def test_divide_by_zero(self):
+        result = self.calc.divide(10, 0)
+        assert result is None
+        
+if __name__ == '__main__':
+    unittest.main()
